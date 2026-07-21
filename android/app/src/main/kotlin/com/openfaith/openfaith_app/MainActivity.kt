ec3e068dbd9e22868967db9baa47c30cd27bdd70
869862742-stack <869862742@qq.com>
2026-07-21 15:30:08 +0800

    feat: register CallBroadcastReceiver MethodChannel in MainActivity

--- android/app/src/main/kotlin/com/openfaith/openfaith_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/openfaith/openfaith_app/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.openfaith.openfaith_app
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import com.openfaith.openfaith_app.services.CallBroadcastReceiver
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        CallBroadcastReceiver.registerChannel(flutterEngine)
+    }
+}
